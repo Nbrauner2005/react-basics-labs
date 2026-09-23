375c92944e66e4e7dd5534132454c31ef1e0a1c3
Your <20107129@setu.ie>
2026-09-23 10:24:53 +0100

    Tasky Lab 3 done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,40 +1,93 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
 const Task = (props) => {
     
     return (
-        <div 
-            className="card"
-            style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}
+           <Grid 
+           key={props.id}
+           size={{ xs: 12, md: 4 }}
         >
-            <p className="title">{props.title}</p>
-            <p>Due: {props.deadline}</p>
-            <p className="description">{props.description}</p>
 
-            <p 
-                className="priority"
-                style={{
-                    backgroundColor:
-                        props.priority === "High" ? "red" :
-                        props.priority === "Medium" ? "#e5aa17" :
-                        "green"
+           <Card
+           sx={{
+            backgroundColor: props.done ? 'lightgrey' : 'lightblue',
+            padding: '20px'
+           }}
+        >
+
+            <CardHeader
+            title={props.title}
+            sx={{
+                backgroundColor: 'white',
+                borderRadius: '3px',
+                padding: '20px',
+                textAlign: 'center'
+            }}
+            />
+
+            <CardContent>
+                <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'baseline',
+                    mb: 2,
+                    padding: '20px'
                 }}
-            >
-                {props.priority}
-            </p>
+                >
+                 <Typography
+                    component="p"
+                    variant="subtitle2"
+                    color="text.primary"
+                    >
+                        Due: {props.deadline}
+                    </Typography>
+                </Box>
 
-            <button 
-                onClick={props.markDone} 
-                className="doneButton"
-            >
-                Done
-            </button>
+                <Typography
+                component="p"
+                variant="subtitle1"
+                align="center"
+                sx={{ fontStyle: 'italic' }}
+                >
+                    {props.description}
+                </Typography>
+            </CardContent>
 
-            <button 
-                className="deleteButton" 
+            <CardActions
+            sx={{
+                justifyContent: 'space-between',
+                padding: '20px'
+            }}
+            >
+                <Button
+                variant="contained"
+                size="small"
+                color="success"
+                onClick={props.markDone}
+                >
+                    Done
+                </Button>
+
+                <Button
+                variant="contained"
+                size="small"
+                color="error"
                 onClick={props.deleteTask}
-            >
-                Delete
-            </button>
-        </div>
+                >
+                    Delete
+                </Button>
+            </CardActions>
+           </Card>
+        </Grid>
+        
     )
 }
 
